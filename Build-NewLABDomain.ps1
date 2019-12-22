a09cef604405ae1ff258ffe06280ab683239f5d5
Jeff Buenting <buenting_jeffrey@bah.com>
2019-12-22 06:20:02 -0500

    added error handling, moved config and DSC files to separate directory

--- Build-NewLABDomain.ps1
+++ Build-NewLABDomain.ps1
@@ -1,26 +1,49 @@
-﻿Connect-VIServer -Server 192.168.1.16 -Credential (Get-Credential)
+﻿Try {
+    Connect-VIServer -Server 192.168.1.16 -Credential (Get-Credential) -ErrorAction Stop
+}
+Catch {
+    $ExceptionMessage = $_.Exception.Message
+    $ExceptionType = $_.Exception.GetType().Fullname
+    Throw "Build-NewLABDomain : Error Connecting to vCenter.`n`n     $ExceptionMessage`n`n $ExceptionType"
+}
 
 $LocalAdmin = Get-Credential -UserName administrator -Message "Servers Local Admin Account"
 
 # ----- Load the Config Data
-. $PSScriptRoot\Config_New-LABDomain.PS1
+. $PSScriptRoot\DSCConfigs\Config_New-LABDomain.PS1
 
 # ----- Dot source New-LABDomain
-. $PSScriptRoot\New-LABDomain.PS1
+. $PSScriptRoot\DSCConfigs\New-LABDomain.PS1
 
 # ----- Build the Config MOF
-New-LABDomain -ConfigurationData $ConfigData `
-    -safemodeAdministratorCred (Get-Credential -UserName '(Password Only)' -Message "New Domain Safe Mode Administrator Password") `
-    -domainCred (Get-Credential -UserName $DomainName\administrator -Message "New Domain Admin Credential") `
-    -OutputPath $PSScriptRoot\MOF -
+try {
+    New-LABDomain -ConfigurationData $ConfigData `
+        -safemodeAdministratorCred (Get-Credential -UserName '(Password Only)' -Message "New Domain Safe Mode Administrator Password") `
+        -domainCred (Get-Credential -UserName $ConfigData.AllNodes.DomainName\administrator -Message "New Domain Admin Credential") `
+        -OutputPath $PSScriptRoot\MOF `
+        -ErrorAction Stop
+}
+Catch {
+    $ExceptionMessage = $_.Exception.Message
+    $ExceptionType = $_.Exception.GetType().Fullname
+    Throw "Build-NewLABDomain : There was a problem building the MOF.`n`n     $ExceptionMessage`n`n $ExceptionType"
+}
 
-# ----- Create the VM.  In this case we are building from a VM Template.  But this could be modified to be from an ISO.
-$VM = New-VM -Name $ConfigData.AllNodes.NodeName -Template $ConfigData.AllNodes.VMTemplate -vmhost $ConfigData.AllNodes.ESXHost
 
-Start-VM -VM $VM
+Try {
+    # ----- Create the VM.  In this case we are building from a VM Template.  But this could be modified to be from an ISO.
+    $VM = New-VM -Name $ConfigData.AllNodes.NodeName -Template $ConfigData.AllNodes.VMTemplate -vmhost $ConfigData.AllNodes.ESXHost -ErrorAction Stop
 
-# ----- reget the VM info.
-$VM = Get-VM -Name $Configdata.AllNodes.NodeName
+    Start-VM -VM $VM -ErrorAction Stop
+
+    # ----- reget the VM info.
+    $VM = Get-VM -Name $Configdata.AllNodes.NodeName -ErrorAction Stop
+}
+Catch {
+    $ExceptionMessage = $_.Exception.Message
+    $ExceptionType = $_.Exception.GetType().Fullname
+    Throw "Build-NewLABDomain : Error building the VM.`n`n     $ExceptionMessage`n`n $ExceptionType"
+}
   
 $IPAddress = $VM.Guest.IpAddress[0]
 
@@ -33,14 +56,13 @@ Copy-Item -Path $PSScriptRoot\mof\$($Configdata.AllNodes.NodeName).mof -Destinat
 COpy-Item -Path $PSScriptRoot\mof\$($Configdata.AllNodes.NodeName).meta.mof -Destination RemoteDrive:\temp\localhost.meta.mof
 
 
-copy-item -path C:\Scripts\lab\MOF\KW-DC1.mof -Destination RemoteDrive:\temp\localhost.mof
-copy-item -path C:\Scripts\lab\MOF\KW-DC1.meta.mof -Destination RemoteDrive:\temp\localhost.meta.mof
+#copy-item -path C:\Scripts\lab\MOF\KW-DC1.mof -Destination RemoteDrive:\temp\localhost.mof
+#copy-item -path C:\Scripts\lab\MOF\KW-DC1.meta.mof -Destination RemoteDrive:\temp\localhost.meta.mof
 
 # ----- We are not using a DSC Pull server so we need to make sure the DSC resources are on the remote computer
 copy-item -path C:\Users\600990\Documents\WindowsPowerShell\Modules\xComputerManagement -Destination "RemoteDrive:\Program Files\WindowsPowerShell\Modules" -Recurse -force
 
-# ----- Push Config MOF to computer
-#Start-DscConfiguration -ComputerName $IPAddress -Path "$PSScriptRoot\mof" -Wait -Verbose
+# ----- Run Config MOF on computer
 $Cmd = "Start-DscConfiguration -path C:\temp -Wait -Verbose -force"
 Invoke-VMScript -VM $VM -GuestCredential $LocalAdmin -ScriptText $CMD 
 
---
+++ DSCConfigs/New-LABDomain.ps1
@@ -0,0 +1,67 @@
+﻿
+configuration New-LABDomain             
+{             
+   param             
+    (                     
+        [Parameter(Mandatory = $true)]             
+        [pscredential]$safemodeAdministratorCred, 
+                    
+        [Parameter(Mandatory = $true)]            
+        [pscredential]$domainCred            
+    )             
+    
+    Import-DscResource –ModuleName 'PSDesiredStateConfiguration'
+    Import-DscResource -ModuleName xActiveDirectory 
+    Import-DscResource -ModuleName xComputerManagement  
+
+    Node $AllNodes.Where{$_.Role -eq "Primary DC"}.Nodename             
+    { 
+
+        LocalConfigurationManager 
+        {
+            ActionAfterReboot = 'ContinueConfiguration'            
+            ConfigurationMode = 'ApplyOnly'            
+            RebootNodeIfNeeded = $true  
+        }
+
+        xComputer SetName { 
+            Name = $Node.NodeName 
+        }
+
+        File ADFiles            
+        {            
+            DestinationPath = 'C:\NTDS'            
+            Type = 'Directory'            
+            Ensure = 'Present'            
+        }            
+                    
+        WindowsFeature ADDSInstall             
+        {             
+            Ensure = "Present"             
+            Name = "AD-Domain-Services"             
+        }            
+            
+        # Optional GUI tools            
+        WindowsFeature ADDSTools            
+        {             
+            Ensure = "Present"             
+            Name = "RSAT-ADDS"             
+        }            
+            
+        # No slash at end of folder paths            
+        xADDomain FirstDS             
+        {             
+            DomainName = $Node.DomainName             
+            DomainAdministratorCredential = $domainCred             
+            SafemodeAdministratorPassword = $safemodeAdministratorCred            
+            DatabasePath = 'C:\NTDS'            
+            LogPath = 'C:\NTDS'            
+            DependsOn = "[WindowsFeature]ADDSInstall","[File]ADFiles"            
+        }            
+    }
+
+}
+           
+            
+ 
+
