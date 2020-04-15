44edeefe21c690ca2e38ebbccc9e1a08956f61ad
Jeff Buenting <buenting_jeffrey@bah.com>
2020-04-15 05:49:25 -0400

    added Horizon View install to DSC Config

--- HorizonView LAB/DSCConfigs/New-ViewConnectionServer.ps1
+++ HorizonView LAB/DSCConfigs/New-ViewConnectionServer.ps1
@@ -62,6 +62,15 @@ configuration New-ViewConnectionServer
 
 
         # ----- Install Connection server
+        # -----https://thevirtualist.org/automated-installation-vmware-view-components/
+        # -----  C:\temp\VMware-Horizon-Connection-Server-x86_64-7.12.0-15770369.exe /s /v "/qn /l c:\temp\viewinstall.log VDM_SERVER_INSTANCE_TYPE=1 INSTALLDIR=""C:\View"" FWCHOICE=1 VDM_SERVER_RECOVERY_PWD=mini VDM_SERVER_RECOVERY_PWD_REMINDER=""yep"""
+        Package HorizonView {
+            Ensure      = "Present"  
+            Path        = "C:\temp\VMware-Horizon-Connection-Server-x86_64-7.12.0-15770369.exe"
+            Name        = "VMware View Connection Server 7.12"
+            ProductId   = "BA8F6334-CB3D-42C2-A6FF-C4A90A3314B7"
+            Arguments   =  '/s /v "/qn /l c:\temp\viewinstall.log VDM_SERVER_INSTANCE_TYPE=1 INSTALLDIR=""C:\View"" FWCHOICE=1 VDM_SERVER_RECOVERY_PWD=mini VDM_SERVER_RECOVERY_PWD_REMINDER=""yep"""'
+        }
 
     }
 }
