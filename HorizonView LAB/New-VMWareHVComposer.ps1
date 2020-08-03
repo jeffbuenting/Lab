426cb21b21354e379f4812f0e0938bca6d8bb826
Jeff Buenting <buenting_jeffrey@bah.com>
2020-08-03 07:43:05 -0400

    initial composer build

---
+++ HorizonView LAB/New-VMWareHVComposer.ps1
@@ -0,0 +1,50 @@
+﻿<#
+    .SYNOPSIS
+        Install Composer service
+    
+#>
+
+
+param (
+    [Parameter (Mandatory = $True)]
+    [String]$ComputerName,
+
+    [String]$SQLServer = $ComputerName,
+
+    [Parameter (Mandatory = $True) ]
+    [PSCredential]$DomainAdminn
+
+)
+
+$ComposerDB = "ComposerDB"
+
+Try {
+    $VM = Get-VM -Name $SQLServer -ErrorAction Stop
+}
+Catch {
+    $ExceptionMessage = $_.Exception.Message
+    $ExceptionType = $_.Exception.GetType().Fullname
+    Throw "New-VMWareHVComposer : Can't get VM.`n`n     $ExceptionMessage`n`n $ExceptionType"
+}
+
+# ----- Create SQL DB for the Composer Service
+Write-Verbose "Creating DB for the COmposer Servce : $ComposerDB"
+
+
+#$CreateDB = @"
+#    # create object and database  
+#  #  $db = New-Object -TypeName Microsoft.SqlServer.Management.Smo.Database -Argumentlist $SQLServer, $ComposerDB 
+#
+# #   write-output ""$($DB | Out-string)""
+#    #$db.Create()  
+#"@
+
+$CreateDB = @"
+    $db = 'Hello'
+"@
+
+Invoke-VMScript -VM $VM -GuestCredential $DomainAdmin -scripttext $CreateDB 
+
+
+
+# ----- Cleanup
