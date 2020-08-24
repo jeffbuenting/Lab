50191a02ab1060a26825ca59abee337d963dd624
Jeff Buenting <buenting_jeffrey@bah.com>
2020-08-24 19:58:17 -0400

    New-VDIMappedDrive

---
+++ HorizonView LAB/Functions/New-VDIMappedDrive.ps1
@@ -0,0 +1,15 @@
+﻿Param (
+    [String]$Path,
+
+    [String]$UserName,
+
+    [String]$Password
+)
+
+$MapDrive = @"
+    $Cred = New-Object System.Management.Automation.PSCredential ($UserName, $(ConvertTo-SecureString $Password -AsPlainText -Force))
+
+    New-PSDrive -Name p -PSProvider FileSystem -Root $Path -Credential $Cred -Persist -ErrorAction stop
+"@
+
+$MapDrive | Out-File -FilePath c:\scripts\mapdrive.ps1
