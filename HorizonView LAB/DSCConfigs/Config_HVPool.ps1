467f79e45bd80441337a5205d1d6b269f13fa5a7
Jeff Buenting <buenting_jeffrey@bah.com>
2020-08-31 06:33:00 -0400

    commented surfp to test second pool build

--- HorizonView LAB/DSCConfigs/Config_HVPool.ps1
+++ HorizonView LAB/DSCConfigs/Config_HVPool.ps1
@@ -1,28 +1,28 @@
 ﻿$ConfigData = @{             
     AllNodes = @(             
-        @{             
-            Nodename = "SurfP"               
-            Role = 'HVPool'  
-            MasterImage = 'WIN10MA'
-            PoolVMFolder = "/KW-HQ/vm/VDI/SurfP"
-            ResourcePool = 'Resources'
-            ESXHost = '192.168.1.15'
-            PoolDataStore = 'NFS-Drobo'
-            PoolNamePattern = "KW-SurfP"
-            PoolMin = 1
-            PoolMax = 2
-            PoolSpare = 1
-            PoolOSCustomization = 'WIN 10 VDI'
-            DomainController = 'KW-DC1'
-            DomainNetBiosName = 'kings-wood'
-            PoolParentOU = 'OU=VDI,DC=kings-wood,DC=local'
-            PoolContainer = "OU=SurfPPool,OU=VDI"
-            EntitledGroup = "SurfP_Users"
-            RetryCount = 20              
-            RetryIntervalSec = 30            
-            PSDscAllowDomainUser = $True
-            PsDscAllowPlainTextPassword = $true  
-        }  
+      #  @{             
+      #      Nodename = "SurfP"               
+      #      Role = 'HVPool'  
+      #      MasterImage = 'WIN10MA'
+      #      PoolVMFolder = "/KW-HQ/vm/VDI/SurfP"
+      #      ResourcePool = 'Resources'
+      #      ESXHost = '192.168.1.15'
+      #      PoolDataStore = 'NFS-Drobo'
+      #      PoolNamePattern = "KW-SurfP"
+      #      PoolMin = 1
+      #      PoolMax = 2
+      #      PoolSpare = 1
+      #      PoolOSCustomization = 'WIN 10 VDI'
+      #      DomainController = 'KW-DC1'
+      #      DomainNetBiosName = 'kings-wood'
+      #      PoolParentOU = 'OU=VDI,DC=kings-wood,DC=local'
+      #      PoolContainer = "OU=SurfPPool,OU=VDI"
+      #      EntitledGroup = "SurfP_Users"
+      #      RetryCount = 20              
+      #      RetryIntervalSec = 30            
+      #      PSDscAllowDomainUser = $True
+      #      PsDscAllowPlainTextPassword = $true  
+      #  }  
         
         @{             
             Nodename = "DtoD"               
